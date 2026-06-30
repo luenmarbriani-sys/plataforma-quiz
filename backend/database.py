ec06bd418c25ff683c67eeee8ab7db908d3b471a
Luenmar Venditti <luenmarbriani@gmail.com>
2026-06-30 20:25:54 -0300

    Configura conexão com PostgreSQL do Railway

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg://postgres:Dl586097luen@localhost/Quizz_edu"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    DATABASE_URL = "postgresql+psycopg://postgres:Dl586097luen@localhost/Quizz_edu"
 
 engine = create_engine(DATABASE_URL)
 
